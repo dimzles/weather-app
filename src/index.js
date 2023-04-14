87bea4f45796bf7f8aa93942d32e0d3c5d5fda30
dimzles <dimzlestv@gmail.com>
2023-04-14 13:43:50 +0100

    Add functions to fetch and format data from api

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 import "./style.css";
 
-console.log("hello world");
+async function formatWeatherData(data) {
+  await data;
+
+  const myData = {
+    location: {
+      country: data.location.country,
+      name: data.location.name,
+      region: data.location.region,
+      localtime: data.location.localtime,
+    },
+    current: {
+      condition: {
+        text: data.current.condition.text,
+        icon: data.current.condition.icon,
+      },
+      temperatures: {
+        temp_c: data.current.temp_c,
+        temp_f: data.current.temp_f,
+        feelsLike_c: data.current.feelslike_c,
+        feelsLike_f: data.current.feelslike_f,
+      },
+      wind: {
+        direction: data.current.wind_dir,
+        wind_mph: data.current.wind_mph,
+        wind_kph: data.current.wind_kph,
+      },
+    },
+  };
+
+  console.log(myData);
+}
+
+async function fetchWeatherData(location) {
+  const response = await fetch(
+    `https://api.weatherapi.com/v1/current.json?key=a960293cd53d42bd911182230230904&q=${location}`
+  );
+
+  const data = await response.json();
+
+  formatWeatherData(data);
+}
+
+fetchWeatherData("london");
