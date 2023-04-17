b66cc7fe81e3fa4d414e1be82591ab92c4a4db85
dimzles <dimzlestv@gmail.com>
2023-04-17 03:30:55 +0100

    Add input for location

--- src/index.js
+++ src/index.js
@@ -1,45 +1,17 @@
 import "./style.css";
+import fetchWeatherData from "./modules/weather";
 
-async function formatWeatherData(data) {
-  await data;
+function handleInput() {
+  const input = document.querySelector("input");
 
-  const myData = {
-    location: {
-      country: data.location.country,
-      name: data.location.name,
-      region: data.location.region,
-      localtime: data.location.localtime,
-    },
-    current: {
-      condition: {
-        text: data.current.condition.text,
-        icon: data.current.condition.icon,
-      },
-      temperatures: {
-        temp_c: data.current.temp_c,
-        temp_f: data.current.temp_f,
-        feelsLike_c: data.current.feelslike_c,
-        feelsLike_f: data.current.feelslike_f,
-      },
-      wind: {
-        direction: data.current.wind_dir,
-        wind_mph: data.current.wind_mph,
-        wind_kph: data.current.wind_kph,
-      },
-    },
-  };
+  fetchWeatherData(input.value);
 
-  console.log(myData);
-}
-
-async function fetchWeatherData(location) {
-  const response = await fetch(
-    `https://api.weatherapi.com/v1/current.json?key=a960293cd53d42bd911182230230904&q=${location}`
-  );
-
-  const data = await response.json();
-
-  formatWeatherData(data);
+  input.value = "";
 }
 
 fetchWeatherData("london");
+
+document.getElementById("search").addEventListener("click", (e) => {
+  e.preventDefault();
+  handleInput();
+});
