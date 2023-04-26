1ccb6113868c3c47b79589fb25f6dc58c0908597
dimzles <dimzlestv@gmail.com>
2023-04-26 16:03:04 +0100

    Add forecast section

--- src/modules/displayController.js
+++ src/modules/displayController.js
@@ -1,4 +1,35 @@
 import fetchWeatherData from "./weather";
+import { getDay } from "date-fns";
+
+function getWeekdayName(date) {
+  const splitDate = date.split("-");
+
+  switch (getDay(new Date(splitDate[0], splitDate[1] - 1, splitDate[2]))) {
+    case 0:
+      return "Sunday";
+      break;
+    case 1:
+      return "Monday";
+      break;
+    case 2:
+      return "Tuesday";
+      break;
+    case 3:
+      return "Wednesday";
+      break;
+    case 4:
+      return "Thursday";
+      break;
+    case 5:
+      return "Friday";
+      break;
+    case 6:
+      return "Saturday";
+      break;
+    default:
+      break;
+  }
+}
 
 async function handleInput() {
   const input = document.querySelector("input");
@@ -10,11 +41,17 @@ async function handleInput() {
   setWeatherInfo(data);
 }
 
+function setHourlyCard(data) {
+  const hourlyCard = document.getElementById("hourly-card");
+  const dailyForecast = data.forecast.hour;
+}
+
 function setLocationCard(data) {
   const cityName = document.getElementById("location");
   const region = document.getElementById("region");
   const condition = document.getElementById("condition");
   const temperature = document.getElementById("temperature-text");
+  const feelslike = document.getElementById("feelslike-temp");
   const conditionImg = document.getElementById("condition-img");
   const timeDisplay = document.getElementById("time-data");
   const time = data.location.localtime.split(" ");
@@ -25,13 +62,13 @@ function setLocationCard(data) {
   region.textContent = data.location.region;
   condition.textContent = data.current.condition.text;
   temperature.textContent = `${data.current.temperatures.temp_c}°C`;
+  feelslike.textContent = `Feels like ${data.current.temperatures.feelsLike_c}°C`;
   conditionImg.src = data.current.condition.icon;
   timeDisplay.textContent = time[1];
 }
 
 function setWindCard(data) {
   const windDirection = document.getElementById("wind-dir");
-  const windArrow = document.getElementById("wind-arrow");
   const windSpeed = document.getElementById("wind-speed");
 
   windDirection.textContent = data.current.wind.direction;
@@ -42,10 +79,59 @@ function setWindCard(data) {
   );
 }
 
+function setForecastCard(data) {
+  const forecastCard = document.getElementById("forecast-card");
+  const forecastH1 = document.createElement("h1");
+  const weeklyForecast = data.weekly;
+
+  forecastCard.textContent = "";
+  forecastH1.classList.add("forecast-h1");
+  forecastH1.textContent = "Forecast";
+  forecastCard.appendChild(forecastH1);
+
+  for (let i = 0; i < weeklyForecast.length; i++) {
+    const forecastContainer = document.createElement("div");
+    const date = document.createElement("h2");
+    const conditionImg = document.createElement("img");
+    const temperature = document.createElement("p");
+    const avgTemperature = document.createElement("p");
+
+    forecastContainer.classList.add("forecast-card");
+    date.classList.add("forecast-date");
+    temperature.classList.add("forecast-temp");
+    avgTemperature.classList.add("forecast-avg-temp");
+
+    switch (i) {
+      case 0:
+        date.textContent = "Today";
+        break;
+      case 1:
+        date.textContent = "Tomorrow";
+        break;
+      default:
+        date.textContent = getWeekdayName(weeklyForecast[i].date);
+        break;
+    }
+
+    conditionImg.src = weeklyForecast[i].day.condition.icon;
+    temperature.textContent = `${weeklyForecast[i].day.mintemp_c}°C - ${weeklyForecast[i].day.maxtemp_c}°C`;
+    avgTemperature.textContent = `Avg: ${weeklyForecast[i].day.avgtemp_c}°C`;
+
+    forecastContainer.appendChild(date);
+    forecastContainer.appendChild(conditionImg);
+    forecastContainer.appendChild(temperature);
+    forecastContainer.appendChild(avgTemperature);
+
+    forecastCard.appendChild(forecastContainer);
+  }
+}
+
 async function setWeatherInfo(data) {
   await data;
   setLocationCard(data);
   setWindCard(data);
+  setHourlyCard(data);
+  setForecastCard(data);
 }
 
-export { handleInput, setWeatherInfo };
+export { handleInput };
