c70415e7fef160e074ed2a26bd71ae8d7b41cbb6
dimzles <dimzlestv@gmail.com>
2023-04-26 01:24:08 +0100

    Clean code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import "./style.css";
-import fetchWeatherData from "./modules/weather";
-import { setWeatherInfo, handleInput } from "./modules/displayController";
+import { handleInput } from "./modules/displayController";
 
 handleInput();
 document.getElementById("search").addEventListener("click", (e) => {
